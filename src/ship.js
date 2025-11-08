ade77323785469484cdb2b97e31c3b192577d22d
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-07 23:20:47 -0800

    Test: Ship class

--- src/ship.js
+++ src/ship.js
@@ -1,16 +1,18 @@
 class Ship {
   #hits;
   constructor(length) {
+    if (typeof length !== "number") throw new TypeError("Length must be a number");
+    if (length < 1) throw new RangeError("Length must be at least 1");
     this.length = length;
     this.#hits = 0;
   }
 
   hit() {
-    
+    this.#hits = Math.min(this.#hits + 1, this.length);
   }
 
   isSunk() {
-    return false;
+    return this.#hits === this.length;
   }
 }
 
--- src/ship.spec.js
+++ src/ship.spec.js
@@ -2,12 +2,29 @@ import Ship from "./ship.js";
 
 describe("public side effects", () => {
   test("length is set", () => expect(new Ship(4).length).toBe(4));
+  test("length type error", () => expect(() => new Ship("4")).toThrow(TypeError));
+  test("length range error", () => expect(() => new Ship(0)).toThrow(RangeError));
 });
 
-describe("hitting", () => {
-  test("hit", () => {
+describe("hitting & sinking", () => {
+  test("hit, no sink", () => {
     const ship = new Ship(3);
     ship.hit();
     expect(ship.isSunk()).toBe(false);
+  });
+  test("hit, sunk", () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+  test("num hits > length", () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
   });
 });
