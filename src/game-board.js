8d90fd17281620d3136d3e788c6285e4754b57b8
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-08 22:30:32 -0800

    Test: placing ships on the game board

---
+++ src/game-board.js
@@ -0,0 +1,65 @@
+import Ship from "./ship";
+
+class GameBoard {
+  static BOARD_LEN = 10;
+  #board;
+  #numShips;
+  constructor() {
+    this.#board = Array.from({ length: GameBoard.BOARD_LEN }, () =>
+      Array.from({ length: GameBoard.BOARD_LEN }, () => new Cell()),
+    );
+  }
+
+  get board() {
+    return [...this.#board];
+  }
+
+  /**
+   *
+   * @param {Integer} col 1 to 10
+   * @param {Integer} row 1 to 10
+   * @param {String} orientation "h" or "v"
+   */
+  placeShip(ship, col, row, orientation) {
+    if (
+      col >= GameBoard.BOARD_LEN ||
+      col < 0 ||
+      row >= GameBoard.BOARD_LEN ||
+      row < 0
+    )
+      throw new RangeError("Index is out of bounds");
+    if (orientation === "h") {
+      if (col + (ship.length - 1) >= GameBoard.BOARD_LEN)
+        throw new RangeError("Ship extends off the board");
+      for (let i = 0; i < ship.length; i++) {
+        const cell = this.#board[col + i][row];
+        if (cell.ship !== null)
+          throw new Error("Ship collides with another ship");
+        cell.ship = ship;
+      }
+    } else if (orientation === "v") {
+      if (row + (ship.length - 1) >= GameBoard.BOARD_LEN)
+        throw new RangeError("Ship extends off the board");
+      for (let i = 0; i < ship.length; i++) {
+        const cell = this.#board[col][row + i];
+        if (cell.ship !== null)
+          throw new Error("Ship collides with another ship");
+        cell.ship = ship;
+      }
+    }
+  }
+
+  receiveAttack(col, row) {
+    const cell = this.#board[col][row];
+    if (cell.ship) cell.ship.hit();
+    cell.isAttacked = true;
+  }
+}
+
+// private member of this module => no need to test
+class Cell {
+  isAttacked = false;
+  ship = null;
+}
+
+export default GameBoard;
---
+++ src/game-board.spec.js
@@ -0,0 +1,125 @@
+import GameBoard from "./game-board";
+import Ship from "./ship";
+
+let gb;
+beforeEach(() => {
+  gb = new GameBoard();
+});
+
+describe("placing ships", () => {
+  describe("no out-of-bounds, no collisions", () => {
+    test("length 2, horizontal", () => {
+      gb.placeShip(new Ship(2), 0, 0, "h");
+      expect(gb.board[0][0].ship).toBeInstanceOf(Ship);
+      expect(gb.board[1][0].ship).toBeInstanceOf(Ship);
+      expect(gb.board[2][0].ship).not.toBeInstanceOf(Ship);
+    });
+
+    test("length 3, vertical", () => {
+      gb.placeShip(new Ship(3), 6, 3, "v");
+      expect(gb.board[6][3].ship).toBeInstanceOf(Ship);
+      expect(gb.board[6][4].ship).toBeInstanceOf(Ship);
+      expect(gb.board[6][5].ship).toBeInstanceOf(Ship);
+      expect(gb.board[6][6].ship).not.toBeInstanceOf(Ship);
+    });
+  });
+
+  describe("error cases: out-of-bounds", () => {
+    describe("arguments are immediately out of range: upper end", () => {
+      test("column case (1)", () => {
+        expect(() => gb.placeShip(new Ship(2), 10, 0, "h")).toThrow(RangeError);
+      });
+      test("column case (2)", () => {
+        expect(() => gb.placeShip(new Ship(2), 11, 0, "h")).toThrow(RangeError);
+      });
+      test("row case (1)", () => {
+        expect(() => gb.placeShip(new Ship(2), 0, 10, "h")).toThrow(RangeError);
+      });
+      test("row case (2)", () => {
+        expect(() => gb.placeShip(new Ship(2), 0, 11, "h")).toThrow(RangeError);
+      });
+    });
+    describe("arguments are immediately out of range: lower end", () => {
+      test("column case (1)", () => {
+        expect(() => gb.placeShip(new Ship(2), -1, 0, "h")).toThrow(RangeError);
+      });
+      test("column case (2)", () => {
+        expect(() => gb.placeShip(new Ship(2), -2, 0, "h")).toThrow(RangeError);
+      });
+      test("row case (1)", () => {
+        expect(() => gb.placeShip(new Ship(2), 0, -1, "h")).toThrow(RangeError);
+      });
+      test("row case (2)", () => {
+        expect(() => gb.placeShip(new Ship(2), 0, -2, "h")).toThrow(RangeError);
+      });
+    });
+
+    describe("ship extends past board", () => {
+      test("horizontal case (1)", () => {
+        expect(() => gb.placeShip(new Ship(3), 8, 0, "h")).toThrow(RangeError);
+      });
+      test("horizontal case (2)", () => {
+        expect(() => gb.placeShip(new Ship(10), 1, 0, "h")).toThrow(RangeError);
+      });
+      test("vertical case (1)", () => {
+        expect(() => gb.placeShip(new Ship(4), 8, 7, "v")).toThrow(RangeError);
+      });
+      test("vertical case (2)", () => {
+        expect(() => gb.placeShip(new Ship(9), 3, 2, "v")).toThrow(RangeError);
+      });
+    });
+  });
+
+  describe("error cases: collision with a ship", () => {
+    beforeEach(() => {
+      gb.placeShip(new Ship(3), 4, 4, "h");
+    });
+
+    describe("placing horizontal", () => {
+      test("case (1)", () => {
+        expect(() => gb.placeShip(new Ship(4), 1, 4, "h")).toThrow(Error);
+      });
+      test("case (2)", () => {
+        expect(() => gb.placeShip(new Ship(2), 4, 4, "h")).toThrow(Error);
+      });
+    });
+    describe("placing vertical", () => {
+      test("case (1)", () => {
+        expect(() => gb.placeShip(new Ship(3), 4, 2, "v")).toThrow(Error);
+      });
+      test("case (2)", () => {
+        expect(() => gb.placeShip(new Ship(5), 4, 1, "v")).toThrow(Error);
+      });
+    });
+  });
+});
+
+describe("receiving attacks", () => {
+  beforeEach(() => {
+    gb.placeShip(new Ship(3), 4, 4, "v");
+  });
+  describe("happy path cases", () => {
+    test("a ship is not sunk", () => {
+      gb.receiveAttack(4, 4);
+      gb.receiveAttack(4, 5);
+      expect(gb.board[4][4].ship.isSunk()).toBe(false);
+    });
+    test("a ship is sunk", () => {
+      gb.receiveAttack(4, 4);
+      gb.receiveAttack(4, 5);
+      gb.receiveAttack(4, 6);
+      expect(gb.board[4][4].ship.isSunk()).toBe(true);
+    });
+    test("miss a shot", () => {
+      gb.receiveAttack(7, 7);
+      expect(gb.board[7][7].isAttacked).toBe(true);
+    });
+  });
+
+  describe("errors", () => {
+    test.todo("bad coordinates");    
+    test.todo("error: already hit this spot: ship present");
+    test.todo("error: already hit this spot: no ship");
+  })
+
+});
