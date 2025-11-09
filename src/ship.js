80fedc7c7e572adafc1b64d9c6a6343cbe7985e9
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-08 22:25:15 -0800

    Refactor: simplify private syntax

--- src/ship.js
+++ src/ship.js
@@ -1,10 +1,9 @@
 class Ship {
-  #hits;
+  #hits = 0;
   constructor(length) {
     if (typeof length !== "number") throw new TypeError("Length must be a number");
     if (length < 1) throw new RangeError("Length must be at least 1");
     this.length = length;
-    this.#hits = 0;
   }
 
   hit() {
