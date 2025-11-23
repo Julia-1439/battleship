2964e3d7c1cbb273875fa123822616388590e0e5
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-22 22:45:44 -0800

    Complete core game logic plus pubsub functionality

--- src/game-control.js
+++ src/game-control.js
@@ -1,8 +1,15 @@
 import { humanPlayer, computerPlayer } from "./player.js";
+import PubSub from "./pub-sub.js";
 
 let player1;
 let player2;
 let turn = null;
+export const pubSub = new PubSub(); // to be used by a UI controller to recognize game events, as defined in `events`
+export const events = { // replace with symbols?
+  GAME_START: "GAME_START",
+  SUCCESSFUL_ATTACK: "SUCCESSFUL_ATTACK", 
+  GAME_END: "GAME_END",
+};
 
 function hasGameBegun() {
   return turn !== null;
@@ -14,30 +21,52 @@ function toggleTurn() {
   turn = turn === 1 ? 2 : 1;
 }
 
-function startGame(player1Name, player2Name) {
+export function createPlayers(player1Name, player2Name) {
+  if (hasGameBegun()) 
+    throw new Error("A game is already in progress")
+
   player1 = humanPlayer(player1Name);
   player2 = computerPlayer();
-  placeShips();
-
-  function placeShips() {
-    player1.gameBoard.placeShip(4, 0, 0, "h");
-    player1.gameBoard.placeShip(2, 4, 4, "v");
-
-    player2.gameBoard.placeShip(4, 6, 0, "h");
-    player2.gameBoard.placeShip(2, 1, 1, "v");
-  }
 }
 
-function attemptAttack(col, row) {
+export function startGame() {
+  if (player1 === undefined || player2 === undefined) 
+    throw new Error("Players must be created first");
+  if (hasGameBegun()) 
+    throw new Error("A game is already in progress")
+
+  placeShips();
+  turn = 1;
+  pubSub.publish(events.GAME_START,);
+}
+
+// will be randomized in the future
+export function placeShips() {
+  player1.gameBoard.placeShip(4, 0, 0, "h");
+  player1.gameBoard.placeShip(2, 4, 4, "v");
+
+  player2.gameBoard.placeShip(4, 6, 0, "h");
+  player2.gameBoard.placeShip(2, 1, 1, "v");
+}
+
+export function playTurn(col, row) {
   if (!hasGameBegun()) throw new Error("A game has not started yet");
   const opponent = turn === 1 ? player2 : player1;
   try {
     opponent.gameBoard.receiveAttack(col, row);
   } catch (err) {
-    return false;
+    throw err;
   }
-  toggleTurn();
-  return true;
+  if (opponent.gameBoard.allShipsSunken()) {
+    endGame();
+    pubSub.publish(events.GAME_END);
+  }
+  
+  toggleTurn(); 
+  pubSub.publish(events.SUCCESSFUL_ATTACK,);
 }
 
-export { player1, player2, hasGameBegun, startGame, playTurn };
+function endGame() { 
+  turn = null;
+}
+
