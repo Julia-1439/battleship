759c03e5f7f5469f70b7cf639b71b04edb945bda
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-30 20:55:27 -0800

    feat: ending popup screen (not functional yet)

--- src/ui-rendering/forms.js
+++ src/ui-rendering/forms.js
@@ -1,39 +1,50 @@
 import * as game from "../game-control.js";
 
-/* ========================================================================== */
-/* FUNCTIONS & VARIABLES */
-/* ========================================================================== */
+(function initSetupForm(doc, window) {
+  const openDialogBtn = doc.querySelector("#setup-game-btn");
+  const dialog = doc.querySelector("#setup-game-dialog");
+  const form = doc.querySelector("#setup-game-form");
+  const cancelBtn = doc.querySelector("#setup-game-form .cancel-btn");
 
-const setup = {
-  openDialogBtn: document.querySelector("#setup-game-btn"),
-  dialog: document.querySelector("#setup-game-dialog"),
-  form: document.querySelector("#setup-game-dialog form"),
-  cancelBtn: document.querySelector("#setup-game-dialog .cancel-btn"),
-};
+  window.addEventListener("load", () => {
+    dialog.showModal();
+  });
+  openDialogBtn.addEventListener("click", () => {
+    dialog.showModal();
+  });
+  form.addEventListener("submit", () => {
+    const formData = new FormData(form);
+    const p1Name = formData.get("p1Name");
+    const p2Name = formData.get("p2Name");
+    game.createPlayers(p1Name, p2Name);
+    game.start(p1Name, p2Name);
+    // choose not to reset the form so the entered info persists if additional games wish to be played
+  });
+  cancelBtn.addEventListener("click", () => {
+    form.reset();
+    dialog.close();
+  });
+})(document, window);
 
-/* ========================================================================== */
-/* LISTENERS */
-/* ========================================================================== */
+(function initEndForm(doc) {
+  const dialog = doc.querySelector("#end-game-dialog");
+  const form = doc.querySelector("#end-game-form");
+  const message = doc.querySelector("#end-game-form p");
+  const cancelBtn = doc.querySelector("#end-game-form .cancel-btn");
 
-window.addEventListener("load", () => {
-  setup.dialog.showModal();
-});
-setup.openDialogBtn.addEventListener("click", () => {
-  setup.dialog.showModal();
-});
-setup.form.addEventListener("submit", () => {
-  const formData = new FormData(setup.form);
-  const player1Name = formData.get("player1Name");
-  const player2Name = formData.get("player2Name");
-  game.createPlayers(player1Name, player2Name);
-  game.start(player1Name, player2Name);
-  setup.form.reset();
-});
-setup.cancelBtn.addEventListener("click", () => {
-  setup.form.reset();
-  setup.dialog.close();
-});
-
-game.pubSub.subscribe(game.events.WINNER_DECLARED, (winner) => {
-  
-});
+  game.pubSub.subscribe(game.events.WINNER_DECLARED, async () => {
+    await setTimeout(() => {
+      dialog.showModal();
+    }, 1000);
+  });
+  game.pubSub.subscribe(game.events.WINNER_DECLARED, (winner) => {
+    message.textContent = `${winner.name ? `Player "${winner.name}"` : "The computer"} has won!`;
+  });
+  form.addEventListener("submit", () => {
+    // @todo
+  });
+  cancelBtn.addEventListener("click", () => {
+    form.reset();
+    dialog.close();
+  });
+})(document);
