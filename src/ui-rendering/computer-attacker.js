a3911696f23eb2b69ba346022c51b93909073a1a
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-12-01 16:58:02 -0800

    docs: a todo to fix a minor bug

--- src/ui-rendering/computer-attacker.js
+++ src/ui-rendering/computer-attacker.js
@@ -7,7 +7,7 @@ const swapScreenBtn = document.querySelector("#swap-screen-btn");
 
 export function initComputerListener() {
   document.addEventListener("custom:p2ScreenVisible", async () => {
-    setStatusMsg("The computer is thinking...");
+    setStatusMsg("The computer is thinking..."); // @todo bug: not displaying
     await delay(THINKING_TIME);
     game.computerPlayTurn();
     swapScreenBtn.dispatchEvent(new Event("click"));
